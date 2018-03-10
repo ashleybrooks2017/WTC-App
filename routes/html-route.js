2863a5e85d6c9802e76020b85e00e2f05a5152d2
Ashley Brooks <30851351+ashleybrooks2017@users.noreply.github.com>
2018-03-10 09:54:00 -0700

    Update html-route.js
    changed the word planner to index

--- routes/html-route.js
+++ routes/html-route.js
@@ -3,11 +3,11 @@ const path = require('path');
 module.exports = function(app)
 {
     app.get('/', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'Planner.html'));
+        res.sendFile(path.join(__dirname, '..', 'index.html'));
     });
 
     app.get('/planner', (req, res) => {
-        res.sendFile(path.join(__dirname, '..', 'Planner.html'));
+        res.sendFile(path.join(__dirname, '..', 'index.html'));
     });
 
     app.get('/organizer', (req, res) => {
